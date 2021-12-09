fdc1b031d15f0a454be0472765e5d6bf757835c5
lok23 <cheng.yang@husky.neu.edu>
2021-12-09 15:08:07 -0500

    got recent_pages unique working

--- server/models/RecentPages.js
+++ server/models/RecentPages.js
@@ -4,15 +4,18 @@ const Schema = mongoose.Schema;
 const RecentPagesSchema = mongoose.Schema({
     userFrom: {
         type: Schema.Types.ObjectId,
-        ref: 'User'
+        ref: 'User',
     },
     movieId : {
         type: String,
-        unique: 1 // we don't want users seeing multiple of the save recently visited pages
     }
 
 }, { timestamps: true })
 
+// This ensures that there are no one user sees has the same movieId twice.
+// https://stackoverflow.com/questions/16061744/mongoose-how-to-define-a-combination-of-fields-to-be-unique
+// Note to self, in order to make changes to the schema, you need to drop the table first before changes will take effect.
+RecentPagesSchema.index({ userFrom: 1, movieId: 1}, { unique: true });
 
 const RecentPages = mongoose.model('RecentPages', RecentPagesSchema);
 
